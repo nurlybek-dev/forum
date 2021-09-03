84125d2255bebfdd581e8cd1e9f62149f7f02b4a
Orynbekov Nurlybek <nurlybek.dev@gmail.com>
2021-09-03 16:14:07 +0600

    update static

--- src/config/settings.py
+++ src/config/settings.py
@@ -128,11 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -151,7 +147,7 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 
-CKEDITOR_BASEPATH = "/staticfiles/ckeditor/ckeditor/"
+CKEDITOR_BASEPATH =  '/static/ckeditor/ckeditor/'
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
 if DEBUG:
